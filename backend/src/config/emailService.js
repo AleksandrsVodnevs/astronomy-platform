5ff1f1fdabb83cbaae12a67ab9d80703e17a9cd8
22DP2AVodn <av00738@rvt.lv>
2026-02-13 19:42:38 +0200

    Fix email port for production

--- backend/src/config/emailService.js
+++ backend/src/config/emailService.js
@@ -1,8 +1,13 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: { user: process.env.EMAIL_USER, pass: process.env.EMAIL_PASS },
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
 });
 
 const sendPasswordResetEmail = async (toEmail, resetToken, firstName) => {
